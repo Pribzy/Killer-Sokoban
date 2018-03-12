8b6d51d7c8de8bb87b52eaa10425c11ec372cf89
Leviathan <32717807+Pribzy@users.noreply.github.com>
2018-03-12 21:05:25 +0100

    Java Classes Added

---
+++ src/therfc/Game.java
@@ -0,0 +1,36 @@
+package therfc;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Game {
+
+    /**
+     * Default constructor
+     */
+    public Game() {
+    }
+
+    /**
+     * 
+     */
+    private Warehouse level;
+
+    /**
+     * @return
+     */
+    public void StartGame() {
+        // TODO implement here
+
+    }
+
+    /**
+     * @return
+     */
+    public void EndGame() {
+        // TODO implement here
+
+    }
+
+}
---
+++ src/therfc/Switch.java
@@ -0,0 +1,51 @@
+package therfc;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Switch extends Field {
+
+    /**
+     * Default constructor
+     */
+    public Switch() {
+    }
+
+    /**
+     * 
+     */
+    private Hole hole;
+
+    /**
+     * @param w 
+     * @param d 
+     * @return
+     */
+    public void StepOn(Worker w, Direction d) {
+        // TODO implement here
+
+    }
+
+    /**
+     * @param b 
+     * @param d 
+     * @return
+     */
+    public void StepOn(Box b, Direction d) {
+        // TODO implement here
+
+    }
+
+    /**
+     * @param h 
+     * @return
+     */
+    public void SetHole(Hole h) {
+        // TODO implement here
+
+    }
+
+
+
+}
