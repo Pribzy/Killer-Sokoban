2fccfffc7eb56446daf3052d8f3995d2a57b4cc8
deviladam <szekely.adam.llg@gmail.com>
2018-04-08 16:39:48 +0200

    IO osztály

--- src/therfc/IO.java
+++ src/therfc/IO.java
@@ -1,4 +1,209 @@
 package therfc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class IO {
+
+    public IO() {
+    }
+
+    public void SetNeighBors(Warehouse wh){
+        for (int i = 0; i <wh.GetFields().size() ; i++) {
+            if(i<20){
+
+                if(i==0){
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 1), Direction.Right);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 20), Direction.Down);
+                }
+                if(i!= 0 && i!=19) {
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 20), Direction.Down);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 1), Direction.Left);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 1), Direction.Right);
+                }
+                if(i==19){
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 20), Direction.Down);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 1), Direction.Left);
+                }
+
+
+
+
+            }
+            if(i>20 && i<380) {
+                if(i%20!=0 && i%20!=19) {
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 1), Direction.Right);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 20), Direction.Down);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 1), Direction.Left);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 20), Direction.Up);
+                }
+                if(i%20==0) {
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 1), Direction.Right);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 20), Direction.Down);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 20), Direction.Up);
+                }
+                if(i%20==19) {
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 1), Direction.Left);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 20), Direction.Down);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 20), Direction.Up);
+                }
+
+            }
+            if(i>=380){
+                if(i==380){
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 1), Direction.Right);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 20), Direction.Up);
+                }
+                if(i!= 380 && i!=399) {
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 20), Direction.Up);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 1), Direction.Left);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i + 1), Direction.Right);
+                }
+                if(i==399){
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 20), Direction.Up);
+                    wh.GetFieldFromIndex(i).SetNeighbor(wh.GetFieldFromIndex(i - 1), Direction.Left);
+                }
+
+            }
+        }
+
+    }
+
+    public Warehouse SetWarehouseFromFile(File level)throws Exception{
+        Warehouse newWarehouse= new Warehouse();
+        Hole hole;
+        for (int k = 0; k < LoadLevel(level).length; k++) {
+            for (int l = 0; l <20 ; l++) {
+                if(LoadLevel(level)[k].charAt(l)=='#'){
+                    newWarehouse.AddField(new Wall());
+                }
+                else  if(LoadLevel(level)[k].charAt(l)=='_'){
+                    newWarehouse.AddField(new SteppableField());
+                }
+                else  if(LoadLevel(level)[k].charAt(l)=='T'){
+                    newWarehouse.AddField(new TargetField());
+                }
+                else  if(LoadLevel(level)[k].charAt(l)=='S'){
+                    Switch s = new Switch();
+                    newWarehouse.AddField(s);
+
+
+                }
+                else  if(LoadLevel(level)[k].charAt(l)=='H'){
+                    hole = new Hole();
+                    hole.SetOpen(true);
+                    newWarehouse.AddField(hole);
+                }
+                else  if(LoadLevel(level)[k].charAt(l)=='C'){
+                    hole = new Hole();
+                    hole.SetOpen(false);
+                    newWarehouse.AddField(hole);
+                }
+
+            }
+        }
+        SetSwitchToHole(level,newWarehouse); //a váltók hozzárendelése a lyukakhoz
+
+        return newWarehouse;
+    }
+    public void SetSwitchToHole(File level,Warehouse wh)throws Exception{ //Hozzárendeli a váltókhoz a lyukakat
+        String s[]=LoadSwitchToHole(level);
+        for (int i = 0; i <SwitchAndHoleCounter(level) ; i++) {
+
+
+            String row[] = s[i].split("->");
+            String sw[] = row[0].split(":"); //switch koordináták
+            String hole[] = row[1].split(":"); //lyuk koordináták
+            Integer mySwitch_x = Integer.parseInt(sw[0]);
+            Integer mySwitch_y = Integer.parseInt(sw[1]);
+            Integer myHole_x = Integer.parseInt(hole[0]);
+            Integer myHole_y = Integer.parseInt(hole[1]);
+            Switch mySwitch = (Switch) wh.GetFieldFromIndex(mySwitch_x + (mySwitch_y * 20));
+            Hole myHole = (Hole) wh.GetFieldFromIndex(myHole_x + (myHole_y * 20));
+            mySwitch.SetHole(myHole);
+        }
+
+
+
+
+    }
+
+
+    public String[] LoadLevel(File level) throws Exception{ //a txt-ből beolvassa a pálya részt
+        BufferedReader br=new BufferedReader(new FileReader(level));
+        String[] switchToHole= new String[SwitchAndHoleCounter(level)];
+        String[] newLevel= new String[20];
+        int i=0;
+        int j=0;
+        while(i< SwitchAndHoleCounter(level)){
+            switchToHole[i]=br.readLine();
+            if(switchToHole[i]==null){
+                break;
+            }
+
+            i++;
+
+        }
+        String separator =br.readLine();
+        i++;
+
+        int to =i+20;
+        while(i< to){
+            newLevel[j]=br.readLine();
+            if(newLevel[j]==null){
+                break;
+            }
+            j++;
+            i++;
+
+        }
+        br.close();
+        return newLevel;
+    }
+    public String[] LoadSwitchToHole(File level) throws Exception{ //a txt-ből beolvassa azt a részt, ahol össze van rendelve a Switch és Hole
+        BufferedReader br=new BufferedReader(new FileReader(level));
+        String[] switchToHole= new String[SwitchAndHoleCounter(level)];
+        String[] newLevel= new String[20];
+        int i=0;
+        int j=0;
+        while(i< SwitchAndHoleCounter(level)){
+            switchToHole[i]=br.readLine();
+            if(switchToHole[i]==null){
+                break;
+            }
+
+            i++;
+
+        }
+        String separator =br.readLine();
+        i++;
+
+        int to =i+20;
+        while(i< to){
+            newLevel[j]=br.readLine();
+            if(newLevel[j]==null){
+                break;
+            }
+            j++;
+            i++;
+
+        }
+        br.close();
+        return switchToHole;
+    }
+
+    public int SwitchAndHoleCounter(File f) throws Exception{ //megszámolja a paraméterül kapott fájlban azokat a sorokat,
+        // ahol a switchek ás lyukak összekapcsolása van
+        BufferedReader br=new BufferedReader(new FileReader(f));
+        int count=0;
+        while(true){
+            String line=br.readLine();
+            if(line.equals("%"))
+                break;
+            count++;
+        }
+        br.close();
+        return count;
+    }
 }
